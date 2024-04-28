c7ed4bef927b668d1cc2de9b762f7833f094910e
alyoung6720 <60701103+alyoung6720@users.noreply.github.com>
2024-04-28 10:22:07 -0400

    Removed udplicate CoRRE sites that are from NutNet since NutNet has most recently updated data.

--- CoDom_SiteLevel_Clim_GDiv_Prod.R
+++ CoDom_SiteLevel_Clim_GDiv_Prod.R
@@ -1,37 +1,42 @@
-setwd("C:/Users/alyoung6/OneDrive - UNCG/SIDE PROJECTS")
+setwd("C:/Users/alyou/OneDrive - UNCG/SIDE PROJECTS")
 
 ### LOAD PACKAGES ###
-library(multcomp)
 library(tidyverse)
 library(codyn)
-library(vegan)
-library(abdiv)
-library(lme4)
-library(lmerTest)
-library(emmeans)
+
 
 #### For 04/01/2024 ####
 ## CoRRE ####
-CoRRERichProd <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/CoRRE/CoRRE_siteBiotic_2021.csv") %>%
+CoRRERichProd <- read.csv("C:/Users/alyou/OneDrive - UNCG/2024_codominance/data/CoRRE/CoRRE_siteBiotic_2021.csv") %>%
   # keep community types w/in projects and sites separate so there are distinct ANPP and richness values for each, but the same coordinates, MAP, and MAT #
   mutate(site_proj_comm = paste(site_code, project_name, community_type, sep="_")) %>%
   rename("GDiv" = "rrich", "ANPP"="anpp")
 #'rrich' is gamma div#
   
-CoRRECoordClim <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/CoRRE/CoRRE_siteLocationClimate_2021.csv") %>%
+CoRRECoordClim <- read.csv("C:/Users/alyou/OneDrive - UNCG/2024_codominance/data/CoRRE/CoRRE_siteLocationClimate_2021.csv") %>%
   # remove Sil and SORBAS sites #
   filter(site_code!="Sil" & site_code!="SORBAS")
 
 CoRREfull <- merge(CoRRECoordClim,CoRRERichProd, by=c("site_code"), all=T) %>%
-  select(site_code, site_proj_comm, Latitude, Longitude, MAP, MAT, GDiv, ANPP)
+  select(site_code, site_proj_comm, Latitude, Longitude, MAP, MAT, GDiv, ANPP) %>%
+  # remove NutNet sites from CoRRE database #
+  filter(site_proj_comm!="CDR_NutNet_0" & 
+           site_proj_comm!="cbgb.us_NutNet_0" &
+           site_proj_comm!="shps.us_NutNet_0" &
+           site_proj_comm!="sier.us_NutNet_0" &
+           site_proj_comm!="temple.us_NutNet_0" &
+           site_proj_comm!="veluwe.nl_NutNet_0" &
+           site_proj_comm!="yarra.au_NutNet_0")
 
 #################
 
 ## GEx ####
-GExfull <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/GEx/GEx-metadata-with-other-env-layers-v2.csv") %>%
+GExfull <- read.csv("C:/Users/alyou/OneDrive - UNCG/2024_codominance/data/GEx/GEx-metadata-with-other-env-layers-v2.csv") %>%
   rename("site_code"="site", "Latitude"="Final.Lat", "Longitude"="Final.Long", "N_deposition"="N.deposition1993",
          "MAP"="precip", "GDiv"="sprich") %>%
-  select(site_code, Latitude, Longitude, MAP, GDiv, ANPP, N_deposition) 
+  # 'bio1' is (MAT*100) from WorldClim in C #
+  mutate(MAT = bio1/10) %>%
+  select(site_code, Latitude, Longitude, MAP, MAT, GDiv, ANPP, N_deposition) 
   # keep 'NA' for Kruger (Mananga, Shitbotawna, and Satara) sites as MAP not good predictor #
   # 'precip' is MAP #
   # 'sprich' is gamma div #
@@ -39,15 +44,20 @@ GExfull <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/GEx
 ################
 
 ## NutNet ####
-NutNetCoordClim <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/NutNet/comb-by-plot-clim-soil_2023-11-07.csv") %>%
+NutNetCoordClim <- read.csv("C:/Users/alyou/OneDrive - UNCG/2024_codominance/data/NutNet/comb-by-plot-clim-soil_2023-11-07.csv") %>%
+  filter(trt=="Control") %>%
   mutate(N_Dep = as.numeric(N_Dep)) %>%
+  # change site_code for 'yarra.au' since the same site_code exists in CoRRE so need to make sure they are not the same experiment #
+  mutate(site_code = ifelse(site_code=="yarra.au", "yarra.au_NutNetdf", site_code)) %>%
   group_by(site_code)%>%
   summarise(Latitude=mean(latitude), Longitude=mean(longitude), GDiv=mean(site_richness),MAP=mean(MAP_v2), MAT=mean(MAT_v2), N_deposition=mean(N_Dep))
 # 'site_richness' is gamma div #
 
-NutNetprod <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/NutNet/full-biomass_2023-11-07.csv") %>% 
+NutNetprod <- read.csv("C:/Users/alyou/OneDrive - UNCG/2024_codominance/data/NutNet/full-biomass_2023-11-07.csv") %>% 
   filter(trt=="Control") %>%
   select(-year_trt, -trt, -live, -subplot, -site_name, -block) %>%
+  # change site_code for 'yarra.au' since the same site_code exists in CoRRE so need to make sure they are not the same experiment (do the same for others after confirm #
+  mutate(site_code = ifelse(site_code=="yarra.au", "yarra.au_NutNetdf", site_code)) %>%
   group_by(year, site_code, plot) %>%
   spread(category,mass) %>%
   rename("FORB_Phlox_diffusa" = "FORB + PHLOX DIFFUSA") %>%
@@ -66,36 +76,7 @@ NutNetprod <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/
 NutNetfULL <- merge.data.frame(NutNetCoordClim, NutNetprod, by=c("site_code"), all=T)
 
 FinalAllNetworks <- bind_rows(CoRREfull, GExfull, NutNetfULL)
-#write.csv(FinalAllNetworks, "C:/Users/alyoung6/OneDrive - UNCG/SIDE PROJECTS/\\CodominanceAllNetworksSummaryAbioticRichProd.csv", row.names=FALSE)
+# write dataframe to csv to then input to ArcGIS Pro and join with HDI raster and NDep raster data from Oak Ridge and export table to .xlsx #
+write.csv(FinalAllNetworks, "C:/Users/alyou/OneDrive - UNCG/SIDE PROJECTS/\\CodominanceAllNetworkData.csv", row.names=FALSE)
 
 
-################################################################################################################################
-#### For 03/25/2024 ####
-# Read in data for CoRRE #
-CoRREcodom <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/CoRRE/corre_codominantsRankAll_202402091.csv")
-CoRRElocat <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/CoRRE/CoRRE_siteLocationClimate_2021.csv")
-CoRRE <- merge(CoRREcodom, CoRRElocat, by=c("site_code"), all=T) %>%
-  rename("year"="calendar_year") %>%
-  select(year, genus_species, site_code, Latitude, Longitude)
-# Read in data for GEx #
-GExcodom <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/GEx/gex_codominantsRankAll_202402091.csv")
-GExlocat <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/GEx/GEx-metadata-with-other-env-layers-v2.csv")
-GEx <- merge(GExcodom, GExlocat, by=c("site"), all=T) %>%
-  rename("site_code"="site", "Latitude"="Final.Lat", "Longitude"="Final.Long") %>%
-  select(year, genus_species, site_code, Latitude, Longitude)
-
-
-# Read in data for NutNet #
-NutNetcodom <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/NutNet/NutNet_codominantsRankAll_20240213.csv") %>%
-  select(site_code, year, genus_species)
-NutNetlocat <- read.csv("C:/Users/alyoung6/OneDrive - UNCG/2024_codominance/data/NutNet/comb-by-plot_2023-11-07.csv") %>%
-  group_by(site_code)%>%
-  summarise(Latitude=mean(latitude), Longitude=mean(longitude))%>%
-  select(site_code, Latitude, Longitude)
-NutNet <- merge(NutNetcodom, NutNetlocat, by=c("site_code"), all=T) %>%
-  select(year, genus_species, site_code, Latitude, Longitude)
-
-
-FinalAllNetworks <- rbind(CoRRE, GEx, NutNet)
-write.csv(FinalAllNetworks, "C:/Users/alyoung6/OneDrive - UNCG/SIDE PROJECTS/\\CodominanceAllNetworkSppLatLon.csv", row.names=FALSE)
-#############################################################################################################################################
